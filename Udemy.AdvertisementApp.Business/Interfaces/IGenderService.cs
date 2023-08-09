129e19dd482622c190576c77303a08f2da160e3e
Ahmet Mücahit Şimşek <itegrisu@gmail.com>
2023-08-09 10:45:56 +0300

    First commit

--- Udemy.AdvertisementApp.Business/Interfaces/IGenderService.cs
+++ Udemy.AdvertisementApp.Business/Interfaces/IGenderService.cs
@@ -7,7 +7,7 @@ using Udemy.AdvertisementApp.Common;
 using Udemy.AdvertisementApp.Dtos;
 using Udemy.AdvertisementApp.Entities;
 
-namespace Udemy.AdvertisementApp.Business.Interfaces
+namespace Udemy.AdvertisementApp.Business.Interfaces 
 {
     public interface IGenderService : IService<GenderCreateDto, GenderUpdateDto, GenderListDto, Gender>
     {
